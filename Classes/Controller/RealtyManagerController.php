8f11065179ffc1abd5c23ccbf62486faafcb3645
chrisgruen <cg@romonta-schach.de>
2021-06-01 19:13:47 +0200

    name ObjectimmosRepository

--- Classes/Controller/RealtyManagerController.php
+++ Classes/Controller/RealtyManagerController.php
@@ -2,26 +2,26 @@
 
 namespace ChrisGruen\RealtyManager\Controller;
 
-use ChrisGruen\RealtyManager\Domain\Repository\ObjectimmosRepository;
+use ChrisGruen\RealtyManager\Domain\Repository\ObjectimmoRepository;
 use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
 
 class RealtyManagerController extends ActionController
 {
-    private $objectimmosRepository;
+    private $objectimmoRepository;
     
     /**
-     * Inject the content repository
+     * Inject the objectimmo repository
      *
-     * @param ChrisGruen\RealtyManager\Domain\Repository\ObjectimmosRepository $objectRepository
+     * @param ChrisGruen\RealtyManager\Domain\Repository\ObjectimmoRepository $objectimmoRepository
      */
-    public function injectObjectRepository(ObjectimmosRepository $objectimmosRepository)
+    public function injectObjectimmoRepository(ObjectimmoRepository $objectimmoRepository)
     {
-        $this->objectimmosRepository = $objectimmosRepository;
+        $this->objectimmoRepository = $objectimmoRepository;
     }
     
     public function listAction()
     {
-        $objects = $this->objectimmosRepository->findAll();
+        $objects = $this->objectimmoRepository->findAll();
         $this->view->assign('objects', $objects);
     } 
     
