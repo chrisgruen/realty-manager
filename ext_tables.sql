dbc007561a868958321c59e746dcb9284af6e2f8
chrisgruen <cg@romonta-schach.de>
2021-06-15 08:31:28 +0200

    modify employer

--- ext_tables.sql
+++ ext_tables.sql
@@ -292,6 +292,7 @@ CREATE TABLE tx_realtymanager_domain_model_districts (
 CREATE TABLE tx_realtymanager_domain_model_employer (
 	uid int(11) unsigned NOT NULL auto_increment,
     pid int(11) DEFAULT '0' NOT NULL,
+    pid_be_user int(11) DEFAULT '0' NOT NULL,
     tstamp int(11) DEFAULT '0' NOT NULL,
     crdate int(11) DEFAULT '0' NOT NULL,
     cruser_id int(11) DEFAULT '0' NOT NULL,
