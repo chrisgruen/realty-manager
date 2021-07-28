6fcb2a670a4a66a2f82a32a17a42dbe7c4bc4485
chrisgruen <cg@romonta-schach.de>
2021-07-28 10:33:16 +0200

    clear utf8

--- Resources/Public/JavaScript/Ajax.js
+++ Resources/Public/JavaScript/Ajax.js
@@ -50,17 +50,6 @@ $(document).ready(function () {
 			}
 	   });
 	});
-	
-	function getFormData($form){
-	    var unindexed_array = $form.serializeArray();
-	    var indexed_array = {};
-
-	    $.map(unindexed_array, function(n, i){
-	        indexed_array[n['name']] = n['value'];
-	    });
-
-	    return indexed_array;
-	}
 
 	$('#ajaxSearchResult').on('click', '.ajax-widget-paginator a', function (e) {
 		e.preventDefault();
